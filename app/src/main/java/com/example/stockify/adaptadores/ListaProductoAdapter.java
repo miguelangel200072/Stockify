76e47f57f2f326da1b4ded0fd11633fb8ca60566
miguelangel200072 <miguelanrg@gmail.com>
2023-05-09 12:43:42 +0200

    ListarProducto eliminar producto

--- app/src/main/java/com/example/stockify/adaptadores/ListaProductoAdapter.java
+++ app/src/main/java/com/example/stockify/adaptadores/ListaProductoAdapter.java
@@ -1,14 +1,18 @@
 package com.example.stockify.adaptadores;
 
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.stockify.R;
+import com.example.stockify.db.EliminarProductoDB;
 import com.example.stockify.entidades.Producto;
 
 import java.util.ArrayList;
@@ -33,6 +37,38 @@ public class ListaProductoAdapter extends RecyclerView.Adapter<ListaProductoAdap
         holder.tvCodigo.setText(listaProducto.get(position).getCodigo());
         holder.tvStand.setText(listaProducto.get(position).getStandNuevo());
 
+        holder.tvDescr.setText(listaProducto.get(position).getDescr());
+        holder.tvCodigo.setText(listaProducto.get(position).getCodigo());
+        holder.tvStand.setText(listaProducto.get(position).getStandNuevo());
+
+        // Agrega el listener para eliminar el producto al hacer clic en el elemento
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Muestra un diálogo de confirmación antes de eliminar el producto
+                new AlertDialog.Builder(holder.itemView.getContext())
+                        .setTitle("Eliminar producto")
+                        .setMessage("¿Está seguro de que desea eliminar este producto?")
+                        .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Elimina el producto de la base de datos y de la lista
+                                EliminarProductoDB eliminarProductoDB = new EliminarProductoDB(holder.itemView.getContext());
+                                boolean eliminado = eliminarProductoDB.eliminarProducto(listaProducto.get(position).getIdProd());
+                                if (eliminado) {
+                                    listaProducto.remove(position);
+                                    notifyItemRemoved(position);
+                                    notifyItemRangeChanged(position, listaProducto.size());
+                                    Toast.makeText(holder.itemView.getContext(), "Producto eliminado", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(holder.itemView.getContext(), "Error al eliminar el producto", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+            }
+        });
     }
 
     @Override
@@ -48,6 +84,7 @@ public class ListaProductoAdapter extends RecyclerView.Adapter<ListaProductoAdap
             tvDescr = itemView.findViewById(R.id.tvDescr);
             tvCodigo = itemView.findViewById(R.id.tvCodigo);
             tvStand = itemView.findViewById(R.id.tvStand);
+
 
         }
     }
---
+++ app/src/main/java/com/example/stockify/db/EliminarProductoDB.java
@@ -0,0 +1,35 @@
+package com.example.stockify.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.annotation.Nullable;
+
+public class EliminarProductoDB extends DatabaseHelper{
+    Context context;
+    private static final String TABLE_PRODUCTO = "producto";
+
+    public EliminarProductoDB(@Nullable Context context) {
+        super(context);
+        this.context = context;
+    }
+    public boolean eliminarProducto(int id) {
+
+        boolean correcto = false;
+
+        DatabaseHelper dbHelper = new DatabaseHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        try {
+            db.execSQL("DELETE FROM " + TABLE_PRODUCTO + " WHERE idProd = '" + id + "'");
+            correcto = true;
+        } catch (Exception ex) {
+            ex.toString();
+            correcto = false;
+        } finally {
+            db.close();
+        }
+
+        return correcto;
+    }
+}
